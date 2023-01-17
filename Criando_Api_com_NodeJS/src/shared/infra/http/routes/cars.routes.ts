2904483d174d3bf7af2d7738ca075b4546881c43
Ianara-cs <ianarasilva333@gmail.com>
2023-01-16 22:43:42 -0300

    testes e o método de listAvailableCars

--- Criando_Api_com_NodeJS/src/shared/infra/http/routes/cars.routes.ts
+++ Criando_Api_com_NodeJS/src/shared/infra/http/routes/cars.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express"
 import { CreateCarController } from "../../../../modules/cars/useCases/createCar/CreateCarController"
+import { ListAvailableCarsController } from "../../../../modules/cars/useCases/listAvailableCars/ListAvailableCarsController"
 import { ensureAdmin } from "../middlewares/ensureAdmin"
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated"
 
 const carsRouter = Router()
 
 const createCarController = new CreateCarController()
+const listAvailableCarsController = new ListAvailableCarsController()
 
 carsRouter.post(
     "/", 
@@ -13,6 +15,8 @@ carsRouter.post(
     ensureAdmin,
     createCarController.handle
 )
+
+carsRouter.get("/available", listAvailableCarsController.handle)
 
 export { carsRouter }
 
