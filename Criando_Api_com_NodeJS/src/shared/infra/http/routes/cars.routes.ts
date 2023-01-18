26df149b35f576bf6bc4065863de24789b4592c6
Ianara-cs <ianarasilva333@gmail.com>
2023-01-18 00:20:05 -0300

    RF: Upload Cars Images

--- Criando_Api_com_NodeJS/src/shared/infra/http/routes/cars.routes.ts
+++ Criando_Api_com_NodeJS/src/shared/infra/http/routes/cars.routes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express"
+import multer from "multer"
+import uploadConfig from '../../../../config/upload'
 import { CreateCarController } from "../../../../modules/cars/useCases/createCar/CreateCarController"
 import { CreateCarSpecificationController } from "../../../../modules/cars/useCases/createCarSpecification/CreateCarSpecificationController"
 import { ListAvailableCarsController } from "../../../../modules/cars/useCases/listAvailableCars/ListAvailableCarsController"
+import { UploadCarImagesController } from "../../../../modules/cars/useCases/uploadCarImage/UploadCarImagesController"
 import { ensureAdmin } from "../middlewares/ensureAdmin"
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated"
 
@@ -10,6 +13,9 @@ const carsRouter = Router()
 const createCarController = new CreateCarController()
 const listAvailableCarsController = new ListAvailableCarsController()
 const createCarSpecificationController = new CreateCarSpecificationController()
+let uploadCarImagesController = new UploadCarImagesController()
+
+const upload = multer(uploadConfig.upload("./tmp/cars"))
 
 carsRouter.post(
     "/", 
@@ -19,11 +25,20 @@ carsRouter.post(
 )
 
 carsRouter.get("/available", listAvailableCarsController.handle)
+
 carsRouter.post(
     "/:id/specifications", 
     ensureAuthenticated,
     ensureAdmin,
     createCarSpecificationController.handle
+)
+
+carsRouter.post(
+    "/images/:id",
+    ensureAuthenticated,
+    ensureAdmin, 
+    upload.array("images"),
+    uploadCarImagesController.handle
 )
 
 export { carsRouter }
