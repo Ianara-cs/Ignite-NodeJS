d0c1c72cbe9e83b6bf82cae94e2e1f7371b0af8e
Ianara-cs <ianarasilva333@gmail.com>
2023-02-27 17:22:47 -0300

    refactor: refresh token e token

--- Criando_Api_com_NodeJS/src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
+++ Criando_Api_com_NodeJS/src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -11,6 +11,11 @@ interface IPayload {
     email: string
 }
 
+interface ITokenResponse {
+    token: string
+    refresh_token: string
+}
+
 @injectable()
 class RefreshTokenUseCase {
     constructor(
@@ -20,7 +25,7 @@ class RefreshTokenUseCase {
         private dateProvider: IDateProvider
     ) {}
 
-    async execute(token: string): Promise<string> {
+    async execute(token: string): Promise<ITokenResponse> {
         const {email, sub: user_id} = verify(token, auth.secret_refresh_token) as IPayload
          
     
@@ -44,8 +49,16 @@ class RefreshTokenUseCase {
             refresh_token,
             user_id
         })
+
+        const newToken = sign({}, auth.secret_token, {
+            subject: user_id,
+            expiresIn: auth.expires_in_token
+        })
         
-        return refresh_token
+        return {
+            refresh_token,
+            token: newToken
+        }
     }
 }
 
